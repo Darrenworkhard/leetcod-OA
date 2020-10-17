f0ab96826af6df067d108478e421cf49a23fd699
Darrenworkhard <hkt.darren.us@gmail.com>
2020-10-16 17:54:24 -0700

    substringofsize

--- Amazon/OA-ABC.java
+++
@@ -1,3 +0,0 @@
-public class OA1-ABC {
-    
-}
---
+++ Amazon/SubstringsofsizeK.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class SubstringsofsizeK{
+    public static void main(String[] args) {
+        List<String> ans = new ArrayList<>();
+        ans = SubstringOfSize("awaglknagawunagwkwagl", 4);
+
+    }
+    public static List<String> SubstringOfSize(String s, int k)
+    {
+        if(s.length() == 0 || k == 0) return new ArrayList<String>();
+        Set<String> ans = new LinkedHashSet<>(); 
+        Set<Character> set = new HashSet<>();
+        char[] cA = s.toCharArray();
+        int i = 0;
+        int j = 0;
+        while(j < cA.length)
+        {
+            if(set.contains(cA[j]))
+            {
+                set.remove(cA[i]);
+                i++;
+            }
+            else if(j - i == k -1)
+            {
+                ans.add(s.substring(i, j+1));
+                set.remove(cA[i]);
+                i++;
+            }
+            else if(j < k + i -1)
+            {
+                set.add(cA[j]);
+                j++;
+
+            }
+        }
+        return new ArrayList<>(ans);
+
+    }
+}
