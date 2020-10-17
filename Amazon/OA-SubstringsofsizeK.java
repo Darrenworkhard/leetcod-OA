7433a0a0d572e8a3e6e2bfa574395bbf617f3835
Darrenworkhard <hkt.darren.us@gmail.com>
2020-10-16 17:10:28 -0700

    init commit

---
+++ Amazon/OA-SubstringsofsizeK.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class SubstringsofsizeK{
+    public static void main(String[] args) {
+        
+    }
+    public static List<String> SubstringOfSize(String s, int k)
+    {
+        Set<Character> set = new HashSet<>();
+        char[] cA = s.toCharArray();
+        int i = 0;
+        int j = k - 1;
+        while(j < cA.length)
+        {
+
+        }
+
+    }
+}
